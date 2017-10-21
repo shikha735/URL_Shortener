9cf5eb9709f467e4c31a071d142881d13b3f2dd4
Shikha Sharma <shikha735@gmail.com>
2017-10-22 03:45:23 +0530

    Updated code

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # URL_Shortener
+
+A console application developed in Python using TinyURL API, to shorten URLs <br>
+Demo: https://youtu.be/IVygSpRBr8I
--- URL Shortener.bat
+++ URL Shortener.bat
@@ -2,4 +2,8 @@
 set /p long_url="Enter URL to shorten: "
 cd build
 main %long_url% > out.txt
+echo Shortening the URL. Please wait...
+echo Saving the output in /build/out.txt 
+echo Short url is:
+type out.txt
 pause
